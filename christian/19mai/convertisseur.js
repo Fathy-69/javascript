b229fd42eea9badde8c3c04d76e2c51cb979ac33
Fathy-69 <ftounekt@gmail.com>
2026-06-04 10:04:47 +0200

    feat: ajout de la fonction de conversion en euro et en dollar

--- christian/19mai/convertisseur.js
+++ christian/19mai/convertisseur.js
@@ -1,3 +1,11 @@
+//NOTE: ce fichier est un module, il peut exporter des fonctions, des variables, des classes, etc. et les rendre disponibles pour d'autres fichiers qui les importent. Il peut également importer des fonctionnalités d'autres modules pour les utiliser dans son propre code.
+//BUG: ce fichier est un module, il peut exporter des fonctions, des variables, des classes, etc. et les rendre disponibles pour d'autres fichiers qui les importent. Il peut également importer des fonctionnalités d'autres modules pour les utiliser dans son propre code.
+//FIXME: ce fichier est un module, il peut exporter des fonctions, des variables, des classes, etc. et les rendre disponibles pour d'autres fichiers qui les importent. Il peut également importer des fonctionnalités d'autres modules pour les utiliser dans son propre code.
+//TODO: convertir en euro et en dollar
+//HACK: export function convertirEnEuro(montant) {
+//     return montant * 0.95;
+// }
+
 export const convertirEnEuro = (montant) => montant * 0.95;
 export const convertirEnDollar = (montant) => montant * 1.10;
 const saluer = (nom) => `Bonjour, ${nom}, ravi de vous revoir !`;
